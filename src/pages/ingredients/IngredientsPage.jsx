c418658bbb83c92fea2f9d1006c525f9830d30a0
Alex Whalen <147879389+Awhalen1999@users.noreply.github.com>
2024-03-16 14:09:06 -0230

    *

--- src/pages/ingredients/IngredientsPage.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const IngredientsPage = () => {
-  return <div>IngredientsPage</div>;
-};
-
-export default IngredientsPage;
--- src/pages/popular-recipes/PopularRecipes.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const PopularRecipes = () => {
-  return <div>PopularRecipes</div>;
-};
-
-export default PopularRecipes;
